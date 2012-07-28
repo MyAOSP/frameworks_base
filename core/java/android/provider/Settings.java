3932d6eaf0abfe97aba0dd55b7fc5e7546e324d8
LorD ClockaN <davor@losinj.com>
2012-07-28 15:38:06 +0200

    Add national data roaming and make it optional (frameworks part 1/2)
        by Robin Morawetz (Rdlgrmpf) and Davor Bertovic (LorD ClockaN)
    
        Change-Id: Ief840d818ecf4b8ebb088fab34f01878d0a9f2d7
    
    Change-Id: I8f7db555600570f0e86db8a5c436d4070ea8a460

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2187,6 +2187,11 @@ public final class Settings {
          */
         public static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
 
+        /*
+         * Whether national data Roming should be used.
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
