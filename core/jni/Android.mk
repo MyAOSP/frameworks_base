07ff226fb4f206ae4533dca5372acdc23fcb368f
travp624 <travp624@gmail.com>
2012-08-22 22:04:29 -0500

    fix moar qcom pollution
    Change-Id: Icd519b32402cef42a46692b5b829cf844411bda8
    
    committed by jt1134

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -164,13 +164,14 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 	    com_android_internal_app_ActivityTrigger.cpp
     LOCAL_C_INCLUDES += \
 	    hardware/qcom/display/libtilerenderer
+    LOCAL_SHARED_LIBRARIES += \
+	    libtilerenderer
 endif
 
 LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE) \
 	$(LOCAL_PATH)/android/graphics \
 	$(LOCAL_PATH)/../../libs/hwui \
-	hardware/qcom/display/libtilerenderer \
 	$(LOCAL_PATH)/../../../native/opengl/libs \
 	$(call include-path-for, bluedroid) \
 	$(call include-path-for, libhardware)/hardware \
@@ -228,12 +229,8 @@ LOCAL_SHARED_LIBRARIES := \
 	libharfbuzz \
 	libz
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	LOCAL_SHARED_LIBRARIES += libtilerenderer
-endif
-
 ifeq ($(USE_OPENGL_RENDERER),true)
-	LOCAL_SHARED_LIBRARIES += libtilerenderer libhwui
+	LOCAL_SHARED_LIBRARIES += libhwui
 endif
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
