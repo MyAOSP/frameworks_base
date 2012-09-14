da38236b4d18911da42f76ba61a194b86230b817
travp624 <travp624@gmail.com>
2012-09-14 12:21:54 -0500

    make notification custom image center_crop this way it fills in landscape

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationWallpaper.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationWallpaper.java
@@ -54,7 +54,7 @@ class NotificationWallpaper extends FrameLayout {
 
         } else if (file.exists()) {
             mNotificationWallpaperImage = new ImageView(getContext());
-            mNotificationWallpaperImage.setScaleType(ScaleType.FIT_XY);
+            mNotificationWallpaperImage.setScaleType(ScaleType.CENTER_CROP);
             addView(mNotificationWallpaperImage, -1, -1);
             bitmapWallpaper = BitmapFactory.decodeFile(NOTIF_WALLPAPER_IMAGE_PATH);
             Drawable d = new BitmapDrawable(getResources(), bitmapWallpaper);
