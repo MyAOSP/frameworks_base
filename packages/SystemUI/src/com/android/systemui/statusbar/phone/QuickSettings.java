a0c6c1f6adf6c28afa0837f425d295998befd3ec
Zaphod <mwilson@acesappraisal.com>
2012-12-04 21:56:46 -0500

    FW - User select Tablet Mode [2/2]
    Allows user to set UI to one of three modes:
    
    Phone/Tablet/Phablet.
    
    Needs RC commit as well.
    
    Patch 2:  Still having lots of resource issues.  I am just uploading
    	this to get a good diff look at what's changed in TabletStatusBar.
    
    Patch3:  Cleanup
    
    Patch 4:  This one at least boots and enables the tablet bar.
    	there is a lot of work still to be done to enable the quick
    	settings panel, etc.
    patch 5:  Implemented the Quick Settings framework.  Haven't tested yet
    	(Well, it compiled) - just posting in case I get busy again,
    	at least someone can take off where I've stopped.
    patch 6: Fix Setting/Notification toggle.
    
    patch 7: Tweak layouts - fix battery/clock
    
    patch 8: Shift Tablet NavRing to left corner
    
    patch 9: add drawables needed for patch 8
    
    patch 10: fix more layouts - various modes now working well
    
    patch 11: Should fix the rest of the notification issues on Tablet mode
    
    patch 12: Fix lockscreen navring and add option to hide compat mode button
    
    patch 13: Re-vamp Notification Title Layout clock/date & buttons layout better
    	  Notif Panel goes to Settings if no Clearable Notifications.
    	  System Bar responds to NavBar size changes in both land/port
    	  Date/Clock actions working
    
    patch 14: Move resources out of sw600/sw720 into "regular" bins. This is
    	  needed for dealing with Tablet Mode on phones (< 600dp).
    
    patch 15: Pull in Steve's Sliding Navbar width adjutments.
    	  also move a few margins into @dimens.  This will need to be
    	  changed on a per device/overlay basis.
    
    patch 16: Fix AokpTarget class into AwesomeAction
    
    patch 17: Move many layout dimens to @dimens files
    	  this allows adjustment/tuning of layouts on a per device
              basis.  Most devices will need to override the dimens.
    
    Change-Id: Ia8d8a7ecaaca232587a54c9f7674936de7b05bda
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>
    
    Conflicts:
    	packages/SystemUI/res/layout-sw600dp/navigation_bar.xml
    	packages/SystemUI/res/layout/system_bar_notification_area.xml
    	packages/SystemUI/res/layout/system_bar_notification_panel_title.xml
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    
    Signed-off-by: Chuck Haines <chaines@codemonkeyjava.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -20,6 +20,7 @@ import com.android.internal.view.RotationPolicy;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.R;
 
+import com.android.systemui.statusbar.BaseStatusBar;
 import com.android.systemui.statusbar.phone.QuickSettingsModel.BluetoothState;
 import com.android.systemui.statusbar.phone.QuickSettingsModel.RSSIState;
 import com.android.systemui.statusbar.phone.QuickSettingsModel.State;
@@ -83,7 +84,7 @@ import java.util.ArrayList;
 /**
  *
  */
-class QuickSettings {
+public class QuickSettings {
     private static final String TAG = "QuickSettings";
     public static final boolean SHOW_IME_TILE = false;
 
@@ -94,7 +95,7 @@ class QuickSettings {
 
     private DisplayManager mDisplayManager;
     private WifiDisplayStatus mWifiDisplayStatus;
-    private PhoneStatusBar mStatusBarService;
+    private BaseStatusBar mStatusBarService;
     private BluetoothState mBluetoothState;
 
     private BrightnessController mBrightnessController;
@@ -161,15 +162,15 @@ class QuickSettings {
         new SettingsObserver(new Handler()).observe();
     }
 
-    void setBar(PanelBar bar) {
+    public void setBar(PanelBar bar) {
         mBar = bar;
     }
 
-    public void setService(PhoneStatusBar phoneStatusBar) {
-        mStatusBarService = phoneStatusBar;
+    public void setService(BaseStatusBar statusBar) {
+        mStatusBarService = statusBar;
     }
 
-    public PhoneStatusBar getService() {
+    public BaseStatusBar getService() {
         return mStatusBarService;
     }
 
@@ -177,7 +178,7 @@ class QuickSettings {
         mModel.onImeWindowStatusChanged(visible);
     }
 
-    void setup(NetworkController networkController, BluetoothController bluetoothController,
+    public void setup(NetworkController networkController, BluetoothController bluetoothController,
             BatteryController batteryController, LocationController locationController) {
         mBluetoothController = bluetoothController;
 
