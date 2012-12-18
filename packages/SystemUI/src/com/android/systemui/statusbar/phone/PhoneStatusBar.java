b444440a3436f22d82911c75f9b3d8bfbe0ae012
travp624 <travp624@gmail.com>
2012-12-18 16:02:43 -0600

    Framework: Forward port Power widget fixes and fix disabling
    This commit fixes the ability to disable/hide the power widget on CM10.1, hide
    the widget by default and forward ports a few other related fixes:
    
    Author: Danesh
    1) Notification Widgets : Fix vibration
    The initial value of the array determines how long till the vibration starts,
    and second value determines how long to leave it on till.
    
    Since config_virtualKeyVibePattern is in an overlay and we cannot rely on its consitency,
    we check the length of the array, and based on it execute the appropriate vibration.
    
    2) NotificationPowerWidget : Snap widgets
    Snap widgets in place, rather than allowing partial widgets
    
    3) Framework Track Control : Switch from broadcast to audio service
    Ordered broadcast is unreliable. Switch to using the Audio Service's
    dispatchMediaKeyWithWakeLockToAudioService as it sends the event to the correct receiver.
    
    4) AutoRotateButton : Minor cleanup
    Use setRotationLock to avoid code duplication and staying consistent with framework
    
    Change-Id: I0b5d1853781a7e6541a4a6283e13f8072d9972b3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -510,6 +510,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                     View.STATUS_BAR_DISABLE_CLOCK);
         }
 
+        // Load the Power widget views and set the listeners
         mPowerWidget = (PowerWidget)mStatusBarWindow.findViewById(R.id.exp_power_stat);
         mPowerWidget.setGlobalButtonOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
@@ -527,7 +528,6 @@ public class PhoneStatusBar extends BaseStatusBar {
         });
 
         mTicker = new MyTicker(context, mStatusBarView);
-
         TickerView tickerView = (TickerView)mStatusBarView.findViewById(R.id.tickerText);
         tickerView.mTicker = mTicker;
 
@@ -665,6 +665,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         updateSettings();
         updateNavigationBarBackground();
         mPowerWidget.setupWidget();
+        mPowerWidget.updateVisibility();
 
         return mStatusBarView;
     }
@@ -1512,7 +1513,8 @@ public class PhoneStatusBar extends BaseStatusBar {
         if (mNotificationButtonAnim != null) mNotificationButtonAnim.cancel();
         if (mClearButtonAnim != null) mClearButtonAnim.cancel();
 
-        mPowerWidget.setVisibility(View.VISIBLE);
+        // Only show the Power widget if it should be shown
+        mPowerWidget.updateVisibility();
 
         mScrollView.setVisibility(View.VISIBLE);
         mScrollViewAnim = start(
@@ -1661,7 +1663,7 @@ public class PhoneStatusBar extends BaseStatusBar {
 
             mScrollView.setScaleX(1f);
             mScrollView.setVisibility(View.VISIBLE);
-            mPowerWidget.setVisibility(View.VISIBLE);
+            mPowerWidget.updateVisibility();
             mSettingsButton.setAlpha(1f);
             mSettingsButton.setVisibility(View.VISIBLE);
             mNotificationPanel.setVisibility(View.GONE);
