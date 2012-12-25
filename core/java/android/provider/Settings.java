65a354f90fbb92fbc1bbacb23645f32995ee934e
travp624 <travp624@gmail.com>
2012-12-25 03:48:57 -0600

    fix missing settings strings.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4157,6 +4157,18 @@ public final class Settings {
         public static final String ADB_ENABLED = Global.ADB_ENABLED;
 
         /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
          * The hostname for this device
          * @hide
          */
