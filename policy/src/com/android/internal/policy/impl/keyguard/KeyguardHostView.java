1d506e85085c36f5afc4dab18e5973261db827d1
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-02 21:48:49 +0100

    LockScreen: Add option to start widgets maximized
    Sources from AOKP with some modifications
    
    This will allow the user to choose wether the lockscreen
    challenge (circle, pattern, pin) is minimized when the screen
    is turned on.
    
    Patch Set 3: Remove visual jump and adhere to setting on initial inflate
    Patch Set 4: Remove accidental dependency
    Patch Set 5: Fix potential multi-user problem.
    
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    Change-Id: Ib5bd0a84e5542a49d34ed8c8541e10a7878a616c

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -249,6 +249,7 @@ public class KeyguardHostView extends KeyguardViewBase {
 
         showPrimarySecurityScreen(false);
         updateSecurityViews();
+        minimizeChallengeIfDesired();
     }
 
     private int getDisabledFeatures(DevicePolicyManager dpm) {
@@ -829,6 +830,7 @@ public class KeyguardHostView extends KeyguardViewBase {
         if (mViewStateManager != null) {
             mViewStateManager.showUsabilityHints();
         }
+        minimizeChallengeIfDesired();
     }
 
     @Override
@@ -913,6 +915,19 @@ public class KeyguardHostView extends KeyguardViewBase {
             // otherwise, go to the unlock screen, see if they can verify it
             mIsVerifyUnlockOnly = true;
             showSecurityScreen(securityMode);
+        }
+    }
+
+    private void minimizeChallengeIfDesired() {
+        if (mSlidingChallengeLayout == null) {
+            return;
+        }
+
+        int setting = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS, 0, UserHandle.USER_CURRENT);
+
+        if (setting == 1) {
+            mSlidingChallengeLayout.showChallenge(false);
         }
     }
 
