3aa6ab3cdb0ebae3dac27af1483300e543166362
travp624 <travp624@gmail.com>
2013-01-16 23:56:30 -0600

    Hardware Keys: cleanup
    Signed-off-by: travp624 <travp624@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2441,8 +2441,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.KILL_APP_LONGPRESS_BACK, 0) == 1) {
                 if (down && repeatCount == 0) {
                     mHandler.postDelayed(mBackLongPress, mBackKillTimeout);
-                } else if (!down) {
-                    mHandler.removeCallbacks(mBackLongPress);
                 }
             }
         }
