162447e82d19c918be807d0d6c6ad12d72ed9e11
baked <travp624@gmail.com>
2014-01-28 05:08:29 -0600

    UI not updated correctly in expanded statusbar
    Sometimes when getting status changes in expanded statusbar the
    text disappear. Both NetworkController and PhoneStatusBar handles
    UI updates and they are not in sync. This fix allows PhoneStatusBar
    to update UI when NetworkController is updating the UI.
    
    Change-Id: I46b8a24ecc732e441f836692c6584a78d8108dff

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -120,7 +120,8 @@ import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
+public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
+        NetworkController.UpdateUIListener {
     static final String TAG = "PhoneStatusBar";
     public static final boolean DEBUG = BaseStatusBar.DEBUG;
     public static final boolean SPEW = false;
@@ -830,6 +831,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mCircleBattery = (BatteryCircleMeterView) mStatusBarView.findViewById(R.id.circle_battery);
         updateBatteryIcons();
 
+        mNetworkController.setListener(this);
+
         return mStatusBarView;
     }
 
@@ -1281,6 +1284,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                 mNotificationIcons.addView(v, i, params);
             }
         }
+    }
+
+    /**
+     * Listen for UI updates and refresh layout.
+     */
+    public void onUpdateUI() {
+        updateCarrierLabelVisibility(true);
     }
 
     protected void updateCarrierLabelVisibility(boolean force) {
