41d82aa9b9d7a45c06b27458254678f8bf4cf008
baked <travp624@gmail.com>
2014-06-01 03:59:19 -0500

    framework: Add equalizer tile
    Shows the current visualizer of current
    audio output of device.
    
    Should not show up for remote-playing device, such
    as Chromecasts.
    
    Clicking the tile opens the current EQ app.
    
    Change-Id: Id7d073699764eacbe1d769a18c249eb80feb2a88
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: baked <travp624@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2860,6 +2860,13 @@ public final class Settings {
          *
          * @hide
          */
+        public static final String QS_DYNAMIC_EQUALIZER = "qs_dynamic_equalizer";
+
+        /**
+         * Quick Settings Panel Dynamic Tiles
+         *
+         * @hide
+         */
         public static final String QS_DYNAMIC_USBTETHER = "qs_dyanmic_usbtether";
 
         /**
